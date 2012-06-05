b3fd01f1dc1446015fc6248dd101d49d615d429b
Ioann Volkov <volkov.ioann@gmail.com>
2012-06-05 23:11:07 +0400

    First version

---
+++ ParserGenerator.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define DEBUG_LOG
+
+struct NonTermRule {
+    int left;
+    std::vector<int>  right;
+    std::vector<bool> rightIsUserCode;
+};
+
+struct TermRule {
+    int left;
+    std::string right;
+};
+
+class Grammar {
+public:
+    Grammar(std::string const& name)
+        : dirName_(name + "_parser") {
+        std::ifstream f(name.c_str());
+        int lineNum = 0;
+        while (!f.eof()) {
+            std::string s;
+            getline(f, s);
+            parseNewRule(s, ++lineNum);
+        }
+        f.close();
+    }
+    
+    ~Grammar() {
+    }
+    
+    void generateParser() {
+        mkdir(dirName_.c_str(), 0777);
+        
+    }
+private:
+    void parseNewRule(std::string const& s, int lineNum) {
+        if (s == "")
+            return;
+        size_t p1 = s.find("->");
+        if (p1 == std::string::npos) {
+            std::cerr << "Rule on line " << lineNum << " incorrect (doesn't have '->')" << std::endl;
+            return;
+        }
+        size_t p2 = s.find("\"");
+        size_t p4 = s.find("{");
+        if (p2 != std::string::npos && p4 == std::string::npos) {
+            size_t p3 = s.find("\"", p2 + 1);
+            if (p3 != std::string::npos) {
+                TermRule newTermRule;
+                newTermRule.left = toNumber(s.substr(0, p1));
+                newTermRule.right = s.substr(p2 + 1, p3 - p2 - 1);
+                termRules_.push_back(newTermRule);
+#ifdef DEBUG_LOG
+                std::cout << "DEBUG: added term rule     [" << newTermRule.left << ": '" << newTermRule.right << "']" << std::endl;
+#endif
+            } else {
+                std::cerr << "Rule on line " << lineNum << " incorrect (has only one [\"])" << std::endl;
+                return;
+            }
+        } else {
+            NonTermRule newNonTermRule;
+            newNonTermRule.left = toNumber(s.substr(0, p1));
+            std::string r = s.substr(p1 + 2);
+            size_t p5, p6;
+            for (int i = 0; i < r.length(); ++i) {
+                switch (r[i]) {
+                    case ' ':
+                        break;
+                    case '{':
+                        p5 = r.find("}", i + 1);
+                        if (p5 != std::string::npos) {
+                            userCode_.push_back(r.substr(i + 1, p5 - i - 1));
+                            newNonTermRule.right.push_back(userCode_.size() - 1);
+                            newNonTermRule.rightIsUserCode.push_back(true);
+                            i = p5;
+                        } else {
+                            std::cerr << "Rule on line " << lineNum << " incorrect (has only '{', no '}')" << std::endl;
+                            return;
+                        }
+                        break;
+                    default:
+                        p6 = r.find(" ", i + 1);
+                        if (p6 != std::string::npos) {
+                            newNonTermRule.right.push_back(toNumber(r.substr(i, p6 - i)));
+                            newNonTermRule.rightIsUserCode.push_back(false);
+                            i = p6;
+                        } else {
+                            newNonTermRule.right.push_back(toNumber(r.substr(i)));
+                            i = r.length();
+                        }
+                        break;
+                }
+            }
+            nonTermRules_.push_back(newNonTermRule);
+#ifdef DEBUG_LOG
+            std::cout << "DEBUG: added non-term rule [" << newNonTermRule.left << ":";
+            for (int i = 0; i < newNonTermRule.right.size(); ++i) {
+                if (newNonTermRule.rightIsUserCode[i]) {
+                    std::cout << " {" << userCode_[newNonTermRule.right[i]] << "}";
+                } else {
+                    std::cout << " " << newNonTermRule.right[i];
+                }
+            }
+            std::cout << std::endl;
+#endif
+        }
+    }
+    
+    int toNumber(std::string const& a) {
+        std::string aCopy = a;
+        while (aCopy[0] == ' ')
+            aCopy = aCopy.substr(1);
+        while (aCopy[aCopy.length() - 1] == ' ')
+            aCopy = aCopy.substr(0, aCopy.length() - 1);
+        if (toNumber_.find(aCopy) != toNumber_.end()) {
+            return toNumber_[aCopy];
+        } else {
+            int newNumber = toNumber_.size();
+            toNumber_[aCopy] = newNumber;
+            return newNumber;
+        }
+    }
+
+    std::string dirName_;
+    std::map<std::string, int> toNumber_;
+    std::vector<NonTermRule> nonTermRules_;
+    std::vector<TermRule> termRules_;
+    std::vector<std::string> userCode_;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: ParserBuilder <grammar_file_name>" << std::endl;
+        return 1;
+    }
+    Grammar g(argv[1]);
+    g.generateParser();
+    return 0;
+}
